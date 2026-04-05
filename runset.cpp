1125edead6131437634498af2a26f9b39f7d782c
2nil <casalalanne@free.fr>
2026-04-05 10:36:53 +0200

    Checked in by dplal from PC-Denis on dim.  5 avr. 2026 10:36:53

--- runset.cpp
+++ runset.cpp
@@ -1,9 +1,13 @@
 
+#include <atomic>
+#include <chrono>
 #include <filesystem>
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <thread>
 #include <vector>
+
 #include <windows.h>
 
 std::wstring to_lower(const std::wstring s)
@@ -13,21 +17,45 @@ std::wstring to_lower(const std::wstring s)
   return d;
 }
 
-std::vector<std::wstring> sws = {L"hide", L"normal", L"showminimized", L"maximize", L"shownoactivate", L"show", L"minimize", L"showminnoactive", L"showna", L"restore", L"showdefault", L"forceminimize"};
+//std::vector<std::wstring> sws = {L"hide", L"normal", L"showminimized", L"maximize", L"shownoactivate", L"show", L"minimize", L"showminnoactive", L"showna", L"restore", L"showdefault", L"forceminimize"};
+std::vector<std::wstring> sws = {{}, L"normal", {}, L"maximize", {}, {}, L"minimize",{}, {}, {}, {}, {}};
 
 int str_to_sw(std::wstring s)
 {
-  int ret = SW_NORMAL;
-
   int i = 0;
   for (std::wstring sw : sws)
   {
-    std::wcout << s << std::endl;
-    if (to_lower(s) == sw) return i;
+    // std::wcout << s << std::endl;
+    if ((to_lower(s) == sw || to_lower(s) == sw.substr(0, 3)) && (i == SW_MAXIMIZE || i == SW_MINIMIZE || i == SW_NORMAL)) return i;
     i++;
   }
 
-  return SW_NORMAL;
+  return SW_HIDE;
+}
+
+void handle_window(HWND hwnd, int x, int y, int w, int h, int show_mode)
+{
+  ShowWindow(hwnd, show_mode);
+  if (show_mode == SW_NORMAL) {
+    MoveWindow(hwnd, x, y, w, h, true);
+    SetForegroundWindow(hwnd);
+  }
+}
+
+HWND g_hwnd = NULL;
+DWORD g_targetPID = 0;
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM /*lParam*/)
+{
+  DWORD pid;
+  GetWindowThreadProcessId(hwnd, &pid);
+
+  if (pid == g_targetPID && GetWindow(hwnd, GW_OWNER) == NULL && IsWindowVisible(hwnd))
+  {
+    g_hwnd = hwnd;
+    return FALSE;
+  }
+  return TRUE;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
@@ -38,31 +66,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
   if (args.size() < 2)
   {
-    std::cerr << "Usage : " << std::filesystem::path(args[0]).stem().string() << R"EOF( [arguments]
-Provide arguments in the following order: program x y w h show_mode
-  prog program to run, at least this argument must be provided and also one of the other should be ...
-  x horizontal position.
-  y vertical position.
-  w width.
-  h height.
-  show mode being one of: hide, normal, showminimized, maximize, shownoactivate, show, minimize, showminnoactive, showna, restore, showdefault, forceminimize
-  (for more detail about show mode, see: https://learn.microsoft.com/windows/win32/api/winuser/nf-winuser-showwindow)
-
-  This program works only for gui apps and of course if the gui app does not force it own position, size and show mode.
+    std::cerr << "Usage : " << std::filesystem::path(args[0]).stem().string() << R"EOF( "Command" [other parameters]
+Provide your parameters respecting the following order: "Command" x y w h show_mode
+This program may only works correctly with gui apps.
+  At least the first parameter must be provided. It corresponds to the app to be run with its eventual parameters and if it contains spaces then it must be surrounded with quotation marks.
+  The other parameters are optional but must respect the order, they correspond to: horizontal position, vertical position, width and height of the window.
+  And finally the show mode of the window may also be provided, being one of: normal (this is the default), minimize or maximize (They may be shorten to min or max). But be aware that by using the minimize or maximize show mode, the provided geometry may not be fully respected when restoring the window.
 )EOF";
 
     return 1;
   }
 
-  bool focus = false;
   STARTUPINFO si = {};
-  si.wShowWindow = SW_NORMAL;
+  si.wShowWindow = SW_MINIMIZE;
   si.dwFlags = STARTF_USESHOWWINDOW;
+  int show_mode=SW_NORMAL;
+  std::wstring sflags = L"", sShow_mode=L"";
+  std::wstringstream ss;
+  ss << "Command: " << args[1];
 
   if (args.size() >= 3)
   {
     si.dwX = std::stoi(args[2]);
     si.dwFlags |= STARTF_USEPOSITION;
+    sflags = L"STARTF_USEPOSITION";
 
     if (args.size() >= 4) si.dwY = std::stoi(args[3]);
 
@@ -70,14 +97,52 @@ Provide arguments in the following order: program x y w h show_mode
     {
       si.dwXSize = std::stoi(args[4]);
       si.dwFlags |= STARTF_USESIZE;
+      sflags += L" | STARTF_USESIZE";
 
       if (args.size() >= 6) si.dwYSize = std::stoi(args[5]);
-      if (args.size() >= 7) si.wShowWindow = str_to_sw(args[6]);
+      if (args.size() >= 7)
+      {
+        si.dwFlags |= STARTF_USESHOWWINDOW;
+        sflags += L" | STARTF_USESHOWWINDOW";
+        show_mode = str_to_sw(args[6]);
+        si.wShowWindow = SW_MINIMIZE; //show_mode;
+        sShow_mode = args[6];
+      }
     }
   }
 
+  if (!sflags.empty())
+    ss << std::endl << "flags: (" << sflags << ") = " << si.dwFlags;
+  if (!sShow_mode.empty())
+    ss << std::endl << "show mode: (" << sShow_mode << ") = " << show_mode;
+  ss << std::endl << "geometry: (" << si.dwX << ", " << si.dwY << ", " << si.dwXSize << ", " << si.dwYSize << ')' << std::endl;
+  std::wcout << ss.str() << std::endl;
+  //MessageBox(nullptr, ss.str().c_str(), L"RunsSet", MB_OK);
   PROCESS_INFORMATION pi;
 
   CreateProcess(nullptr, args[1].wstring().data(), nullptr, nullptr, FALSE, 0, nullptr, nullptr, &si, &pi);
-  return 0;
+
+  g_targetPID = pi.dwProcessId;
+
+  auto start = std::chrono::steady_clock::now();
+
+  while (g_hwnd == nullptr) {
+    EnumWindows(EnumWindowsProc, 0);
+    if (std::chrono::steady_clock::now() - start > std::chrono::seconds(5)) break;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  if (g_hwnd)
+  {
+    wchar_t s[1024];
+    GetWindowText(g_hwnd, s, 1024);
+    std::wcout << "Found window with handle: " << g_hwnd << " and name: " << s << std::endl;
+    handle_window(g_hwnd, si.dwX, si.dwY, si.dwXSize, si.dwYSize, show_mode);
+    return 0;
+  }
+  else
+  {
+    std::cout << "Window not found" << std::endl;
+    return 1;
+  }
 }
--- runset_example.cmd
+++ runset_example.cmd
@@ -1,3 +1,5 @@
-PATH build\msvc\win\x64\Release;C:\Program Files\Vim\vim92
-runset gvim 10 10 600 400 maximize
-runset gvim 2200 1200 600 400
+PATH %PATH%;C:\Users\dplal\Documents\home\01-runset\build\msvc\win\x64\Release;C:\Program Files\Vim\vim92
+set EDITOR=gvim
+set FDIR=C:\Users\dplal\Documents\home\01-runset
+::runset "%EDITOR% %FDIR%\runset.svg" 10 10 600 400 maximize
+runset "%EDITOR% %FDIR%\runset.rc" 1200 500 300 200
